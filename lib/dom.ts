164d27aaf7392b8973e866bff85e27700a8dff9f
Turtlemay <am@trtl.in>
2021-06-01 22:21:37 -0400

    Indent with tabs.

--- lib/dom.ts
+++ lib/dom.ts
@@ -3,16 +3,16 @@
  */
 
 export function focusInputAtEnd(elem: HTMLInputElement) {
-  elem.focus()
-  const supportsSelection = (
-    elem?.type === 'text' ||
-    elem?.type === 'search' ||
-    elem?.type === 'URL' ||
-    elem?.type === 'tel' ||
-    elem?.type === 'password'
-  )
-  if (supportsSelection) {
-    const position = elem.value.length
-    elem.setSelectionRange(position, position)
-  }
+	elem.focus()
+	const supportsSelection = (
+		elem?.type === 'text' ||
+		elem?.type === 'search' ||
+		elem?.type === 'URL' ||
+		elem?.type === 'tel' ||
+		elem?.type === 'password'
+	)
+	if (supportsSelection) {
+		const position = elem.value.length
+		elem.setSelectionRange(position, position)
+	}
 }
--- types.d.ts
+++ types.d.ts
@@ -7,20 +7,20 @@ declare const __BUILD_DATE__: string | undefined
 type IItemDbInfo = Pick<IItemDb, 'name' | 'version' | 'organization'>
 
 interface IItemDb {
-  name: string
-  version: string
-  organization?: string
-  items: IItemData[]
+	name: string
+	version: string
+	organization?: string
+	items: IItemData[]
 }
 
 interface IItemData {
-  'priority-keywords'?: string[]
-  alwaysShowBarcode?: boolean
-  duplicate?: boolean
-  ignore?: boolean
-  keywords?: string[]
-  name: string
-  tags?: string[]
-  uiColor?: string
-  value: number | string
+	'priority-keywords'?: string[]
+	alwaysShowBarcode?: boolean
+	duplicate?: boolean
+	ignore?: boolean
+	keywords?: string[]
+	name: string
+	tags?: string[]
+	uiColor?: string
+	value: number | string
 }
