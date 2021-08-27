e7e5ca2dfd1aaa0bd23a5d579a24da699ff5101b
Turtlemay <am@trtl.in>
2021-08-27 07:52:42 -0400

    Optional query prop.

--- components/item-cards.tsx
+++ components/item-cards.tsx
@@ -4,7 +4,7 @@ import { AppStateContext } from './AppStateProvider'
 import c from 'classnames'
 
 export function StoreItemCard(props: {
-	query: string
+	query?: string
 	data: IItemData
 	onPick?: (jsx: JSX.Element) => void
 }) {
@@ -14,7 +14,7 @@ export function StoreItemCard(props: {
 	let value = String(props.data.value)
 
 	// Add organic prefix.
-	if (context.organicModifier && props.query.endsWith(context.organicModifier) && value.length === 4 && !name.includes('organic')) {
+	if (context.organicModifier && props.query?.endsWith(context.organicModifier) && value.length === 4 && !name.includes('organic')) {
 		name = `[Organic] ${name}`
 		value = '9' + value
 	}
