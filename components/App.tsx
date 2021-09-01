70a7d4e18989070bae0f111bf83ca3595a8e239b
Turtlemay <am@trtl.in>
2021-09-01 11:20:25 -0400

    Getting key combo from context.

--- components/App.tsx
+++ components/App.tsx
@@ -3,10 +3,11 @@ import c from 'classnames'
 import { HashRouter, NavLink, Route, Switch, RouteComponentProps, matchPath, Redirect, useHistory } from 'react-router-dom'
 import { MainView } from './MainView'
 import { PrefsView } from './PrefsView'
-import { AppStateProvider } from './AppStateProvider'
+import { AppStateContext, AppStateProvider } from './AppStateProvider'
 import { InfoView } from './InfoView'
 import { WeightCalcView } from './WeightCalcView'
 import { TransitionGroup, CSSTransition } from 'react-transition-group'
+import { matchKeyCombo } from '../src/keys'
 
 export const App = () => (
 	<HashRouter>
@@ -18,44 +19,25 @@ export const App = () => (
 )
 
 const AppMainRouteComponent = (props: RouteComponentProps) => {
-	const history = useHistory()
 	const matchedMainView = Boolean(matchPath(props.location.pathname, { path: '/l' }))
-
-	React.useEffect(updateKeyListener)
-
-	function updateKeyListener() {
-		addEventListener('keydown', handleKeyDown)
-
-		return function cleanup() {
-			removeEventListener('keydown', handleKeyDown)
-		}
-
-		function handleKeyDown(e: KeyboardEvent) {
-			if (e.shiftKey && e.code === 'Space') {
-				e.preventDefault()
-				history.push('/')
-			}
-		}
-	}
-
 	return (
 		<div className="app__layout">
-
 			<AppStateProvider>
-				<div className="app__layoutMain app__viewContainer">
-					<TransitionGroup component={null}>
-						<MainView className={c('app__viewTransition', { 'active': matchedMainView })} active={matchedMainView} />
-						<CSSTransition classNames="appViewTransitionAnimation" timeout={250} key={props.location.pathname}>
-							<Switch location={props.location}>
-								<Route exact path="/prefs" component={PrefsView} />
-								<Route exact path="/info" component={InfoView} />
-								<Route exact path="/wcalc" component={WeightCalcView} />
-							</Switch>
-						</CSSTransition>
-					</TransitionGroup>
-				</div>
+				<AppStateConsumer>
+					<div className="app__layoutMain app__viewContainer">
+						<TransitionGroup component={null}>
+							<MainView className={c('app__viewTransition', { 'active': matchedMainView })} active={matchedMainView} />
+							<CSSTransition classNames="appViewTransitionAnimation" timeout={250} key={props.location.pathname}>
+								<Switch location={props.location}>
+									<Route exact path="/prefs" component={PrefsView} />
+									<Route exact path="/info" component={InfoView} />
+									<Route exact path="/wcalc" component={WeightCalcView} />
+								</Switch>
+							</CSSTransition>
+						</TransitionGroup>
+					</div>
+				</AppStateConsumer>
 			</AppStateProvider>
-
 			<div className="app__layoutBottom app__navbarContainer">
 				<nav className="app__navbar">
 					<NavLink className="app__navItem" activeClassName="app__navItem--active" to="/l">
@@ -72,7 +54,26 @@ const AppMainRouteComponent = (props: RouteComponentProps) => {
 					</NavLink>
 				</nav>
 			</div>
-
 		</div>
 	)
+}
+
+function AppStateConsumer(props: React.PropsWithChildren<{}>) {
+	const history = useHistory()
+	const context = React.useContext(AppStateContext)
+
+	React.useEffect(updateKeyListener)
+
+	function updateKeyListener() {
+		addEventListener('keydown', fn)
+		return () => removeEventListener('keydown', fn)
+		function fn(e: KeyboardEvent) {
+			if (matchKeyCombo(e, context.appRestartKey)) {
+				e.preventDefault()
+				history.push('/')
+			}
+		}
+	}
+
+	return <React.Fragment children={props.children} />
 }
