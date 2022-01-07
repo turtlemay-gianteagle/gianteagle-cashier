844cde2dfd9be355d2b698894c7f6d589b955d46
Turtlemay <am@trtl.in>
2022-01-06 22:38:42 -0500

    Updated text.

--- components/InfoView.tsx
+++ components/InfoView.tsx
@@ -19,6 +19,7 @@ export const InfoView = () => (
 			<section>
 				<h2>📅 Recent Changes</h2>
 				<ul>
+					<li>Added experimental voice input for supported browsers.</li>
 					<li>Added compact barcode option.</li>
 					<li>Now supporting multiple organizations. Set your database in settings.</li>
 					<li>Support key combinations in settings.</li>
